c659f8a9ae975f57a950a8c309c9ec84ef7ad0dc
VU Tuan Trung <trung.lespace@gmail.com>
2020-04-24 16:15:45 +0200

    Remove unnecessary method

--- DesignPattern/FactoryMethod/Creator/ConcreteCreator1.cs
+++ DesignPattern/FactoryMethod/Creator/ConcreteCreator1.cs
@@ -5,15 +5,11 @@ using System.Text;
 
 namespace FactoryMethod.Class
 {
-    class ConcreteCreator1 : Creator
+    public class ConcreteCreator1 : Creator
     {
-        IProduct _product;
-
         public override IProduct FactoryMethod()
         {
             return new ConcreteProduct1();
         }
-
-        public ProductTypes GetValueProduct => _product.GetValue();
     }
 }
