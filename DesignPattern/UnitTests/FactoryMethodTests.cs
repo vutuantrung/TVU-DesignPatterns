c9d5b4ba418071b2b6577fad88b40089f242fee2
VU Tuan Trung <trung.lespace@gmail.com>
2020-04-24 16:18:14 +0200

    Add Factory method unittest

---
+++ DesignPattern/UnitTests/FactoryMethodTests.cs
@@ -0,0 +1,33 @@
+﻿using FactoryMethod.Class;
+using FactoryMethod.Products;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class FactoryMethodTests
+    {
+        [Test]
+        public void get_product_1()
+        {
+            var creator = new ConcreteCreator1();
+
+            var product = creator.FactoryMethod();
+
+            Assert.That( product.GetValue() == ProductTypes.ValueProduct1 );
+        }
+
+        [Test]
+        public void get_product_2()
+        {
+            var creator = new ConcreteCreator2();
+
+            var product = creator.FactoryMethod();
+
+            Assert.That( product.GetValue() == ProductTypes.ValueProduct2 );
+        }
+    }
+}
