28d50125d2e8ea043a85da2f7deb79211eebdbb2
VU Tuan Trung <trung.lespace@gmail.com>
2020-04-26 14:32:51 +0200

    Add product A class and their interface class

---
+++ DesignPattern/FactoryAbstract/Products/IAbstractProductA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryAbstract.Products
+{
+    public interface IAbstractProductA
+    {
+        ProductsType GetValue();
+
+        int GetProductNumber();
+    }
+}
