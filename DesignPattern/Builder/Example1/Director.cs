e0bbb10ae7f71f7293a2b7bc6d5877ff1e74cfa6
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-03 14:38:44 +0200

    Create Director

---
+++ DesignPattern/Builder/Example1/Director.cs
@@ -0,0 +1,29 @@
+﻿using Builder.Example1.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Example1
+{
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        public void BuildMinimalViableProduct()
+        {
+            _builder.BuilderPartA();
+        }
+
+        public void BuildFullFeatureProduct()
+        {
+            _builder.BuilderPartA();
+            _builder.BuilderPartB();
+            _builder.BuilderPartC();
+        }
+    }
+}
