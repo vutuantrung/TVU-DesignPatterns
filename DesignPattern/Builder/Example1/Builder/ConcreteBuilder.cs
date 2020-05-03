96a2128c8f6b48f48249c92a1157f42226150572
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-03 14:38:16 +0200

    Create Builder and its interface class

---
+++ DesignPattern/Builder/Example1/Builder/ConcreteBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Builder.Example1.Products;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Example1.Builder
+{
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+
+        public ConcreteBuilder() 
+        {
+            _product = new Product();
+        }
+
+        public void Reset()
+        {
+            _product = new Product();
+        }
+
+        public void BuilderPartA()
+        {
+            _product.Add( "Part A1" );
+        }
+
+        public void BuilderPartB()
+        {
+            _product.Add( "Part B1" );
+        }
+
+        public void BuilderPartC()
+        {
+            _product.Add( "Part C1" );
+        }
+
+        public Product GetProduct()
+        {
+            Product result = _product;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+}
