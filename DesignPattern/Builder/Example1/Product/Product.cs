b273c6efb08d1114d0923f14cbb43e76196d0a3a
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-03 14:38:31 +0200

    Create Product class

---
+++ DesignPattern/Builder/Example1/Product/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Example1.Products
+{
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+
+        public void Add( string part )
+        {
+            _parts.Add( part );
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for ( int i = 0; i < _parts.Count; i++ )
+            {
+                str += _parts[i] + ", ";
+            }
+
+            str = str.Remove( str.Length - 2 );
+
+            return $"Product parts: { str }.";
+        }
+
+    }
+}
