20a063607cbe65cc81fc593c8255037946f7bcd4
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-09 16:35:28 +0200

    Update Product class

--- DesignPattern/Builder/Example1/Product/Product.cs
+++ DesignPattern/Builder/Example1/Product/Product.cs
@@ -6,26 +6,24 @@ namespace Builder.Example1.Products
 {
     public class Product
     {
-        private List<object> _parts = new List<object>();
+        private List<object> _parts;
 
-        public void Add( string part )
+        private int _count;
+
+        public Product()
+        {
+            _parts = new List<object>();
+            _count = 0;
+        }
+
+        public int Count => _parts.Count;
+
+        public List<object> ListPart => _parts;
+
+        public void Add( object part )
         {
             _parts.Add( part );
+            _count++;
         }
-
-        public string ListParts()
-        {
-            string str = string.Empty;
-
-            for ( int i = 0; i < _parts.Count; i++ )
-            {
-                str += _parts[i] + ", ";
-            }
-
-            str = str.Remove( str.Length - 2 );
-
-            return $"Product parts: { str }.";
-        }
-
     }
 }
