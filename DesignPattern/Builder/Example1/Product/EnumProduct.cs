3c0101f0ed17dda70c5ecfdfb1165b66d483b09c
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-09 16:34:59 +0200

    Add product enum

---
+++ DesignPattern/Builder/Example1/Product/EnumProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Example1.Products
+{
+    public enum EnumProduct_1
+    {
+        PartA,
+        PartB,
+        PartC,
+        PartD,
+        PartE
+    }
+
+    public enum EnumProduct_2
+    {
+        PartA,
+        PartB,
+        PartC,
+        PartD
+    }
+}
