cc07503d4881db5127fa1c96139235064a228090
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-10 02:27:46 +0200

    Add 3 classes for person information

---
+++ DesignPattern/Prototype/PersonInfo/PersonAdress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype.PersonInfo
+{
+    public class PersonAdress
+    {
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string District { get; set; }
+
+        public string Street { get; set; }
+
+        public int Number { get; set; }
+    }
+}
---
+++ DesignPattern/Prototype/PersonInfo/PersonRelationship.cs
@@ -0,0 +1,18 @@
+﻿using Prototype.Enum;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Prototype.PersonInfo
+{
+    public class PersonRelationship
+    {
+        public EnumRelationship Relationship { get; set; }
+
+        public PersonRelationship(EnumRelationship relationShip)
+        {
+            Relationship = relationShip;
+        }
+    }
+}
