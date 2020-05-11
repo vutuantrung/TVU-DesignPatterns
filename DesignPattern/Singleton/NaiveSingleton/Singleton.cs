0cad1953ac51970b19611a98fddff1a34624f2c0
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-11 20:38:14 +0200

    Add naive singleton

---
+++ DesignPattern/Singleton/NaiveSingleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.NaiveSingleton
+{
+    public class Singleton
+    {
+        // The Singleton's constructor should always be private to prevent direct construction calls with the `new` operator.
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if ( _instance == null )
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
