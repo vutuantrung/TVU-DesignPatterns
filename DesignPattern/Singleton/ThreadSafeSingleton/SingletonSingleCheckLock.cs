8366f9c650fdc9234c3286e9d578986623df3037
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-13 11:38:31 +0200

    Modify Singleton double check class

--- DesignPattern/Singleton/ThreadSafeSingleton/SingletonSingleCheckLock.cs
+++ DesignPattern/Singleton/ThreadSafeSingleton/SingletonSingleCheckLock.cs
@@ -4,38 +4,45 @@ using System.Text;
 
 namespace Singleton.ThreadSafeSingleton
 {
-    class SingletonSingleCheckLock
+    public sealed class SingletonSingleCheckLock : ISingletonDemo
     {
-        private SingletonSingleCheckLock()
-        {
-
-        }
-
-        private static SingletonSingleCheckLock _instance;
+        private static int _counter = 0;
 
         private static readonly object _lock = new object();
 
-        public static SingletonSingleCheckLock GetInstance( string value )
+        private static SingletonSingleCheckLock _instance;
+
+        public static SingletonSingleCheckLock GetInstance
         {
             // Now, imagine that the program has just been launched. 
             // Since there's no Singleton instance yet, multiple threads can simultaneously pass the previous conditional and reach this point almost at the same time.
             // The first of them will acquire lock and will proceed further, while the rest will wait here.
-            lock ( _lock )
+            get
             {
-
-                // The first thread to acquire the lock, reaches this conditional, goes inside and creates the Singleton instance.
-                // Once it leaves the lock block, a thread that might have been waiting for the lock release may then enter this section.
-                // But since the Singleton field is already initialized, the thread won't create a new object.
-                if ( _instance == null )
+                lock ( _lock )
                 {
-                    _instance = new SingletonSingleCheckLock();
-                    _instance.Value = value;
+                    // The first thread to acquire the lock, reaches this conditional, goes inside and creates the Singleton instance.
+                    // Once it leaves the lock block, a thread that might have been waiting for the lock release may then enter this section.
+                    // But since the Singleton field is already initialized, the thread won't create a new object.
+                    if ( _instance == null )
+                    {
+                        _instance = new SingletonSingleCheckLock();
+                    }
+
+                    return _instance;
                 }
             }
-
-            return _instance;
         }
 
-        public string Value { get; set; }
+        private SingletonSingleCheckLock()
+        {
+            _counter++;
+            Console.WriteLine( $"Counter: { _counter }" );
+        }
+
+        public void PrintInformation( string message )
+        {
+            Console.WriteLine( message );
+        }
     }
 }
