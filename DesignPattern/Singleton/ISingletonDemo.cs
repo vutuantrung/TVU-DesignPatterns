ac0bd5ffa939dc65748bb5161d207ec30217b098
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-13 11:39:10 +0200

    Create interface for all Singleton

---
+++ DesignPattern/Singleton/ISingletonDemo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public interface ISingletonDemo
+    {
+        void PrintInformation( string message );
+    }
+}
