81bc3e6b26d9b269a2a05ab60f087c797ff6ea1e
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-13 11:38:48 +0200

    Add Singleton counter

---
+++ DesignPattern/Singleton/NaiveSingleton/SingletonCounter.cs
@@ -0,0 +1,38 @@
+﻿using Singleton.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.NaiveSingleton
+{
+    public sealed class SingletonCounter : ISingletonDemo
+    {
+        private static int _counter = 0;
+
+        private static SingletonCounter instance = null;
+
+        public static SingletonCounter GetInstance
+        {
+            get
+            {
+                if ( instance == null )
+                {
+                    instance = new SingletonCounter();
+                }
+
+                return instance;
+            }
+        }
+
+        public void PrintInformation( string message )
+        {
+            Console.WriteLine( message );
+        }
+
+        private SingletonCounter()
+        {
+            _counter++;
+            Console.WriteLine( $"Counter: { _counter }" );
+        }
+    }
+}
