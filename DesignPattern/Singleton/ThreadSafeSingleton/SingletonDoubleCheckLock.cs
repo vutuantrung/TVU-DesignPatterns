184bf894f6da3f14cbfd1a8e318f3b2467d645f6
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-13 11:38:16 +0200

    Modify Singleton single check class

--- DesignPattern/Singleton/ThreadSafeSingleton/SingletonDoubleCheckLock.cs
+++ DesignPattern/Singleton/ThreadSafeSingleton/SingletonDoubleCheckLock.cs
@@ -4,43 +4,49 @@ using System.Text;
 
 namespace Singleton.ThreadSafeSingleton
 {
+    //SingletonDoubleCheckLock
     // Double check locking
-    class SingletonDoubleCheckLock
+    public sealed class SingletonDoubleCheckLock : ISingletonDemo
     {
-        private SingletonDoubleCheckLock()
-        {
-
-        }
-
-        private static SingletonDoubleCheckLock _instance;
+        private static int _counter = 0;
 
         private static readonly object _lock = new object();
 
-        public static SingletonDoubleCheckLock GetInstance( string value )
-        {
-            // prevent threads stumbling over the lock once the instance is ready.
-            if ( _instance == null )
-            {
-                // Now, imagine that the program has just been launched. 
-                // Since there's no Singleton instance yet, multiple threads can simultaneously pass the previous conditional and reach this point almost at the same time.
-                // The first of them will acquire lock and will proceed further, while the rest will wait here.
-                lock ( _lock )
-                {
+        private static SingletonDoubleCheckLock _instance;
 
-                    // The first thread to acquire the lock, reaches this conditional, goes inside and creates the Singleton instance.
-                    // Once it leaves the lock block, a thread that might have been waiting for the lock release may then enter this section.
-                    // But since the Singleton field is already initialized, the thread won't create a new object.
-                    if ( _instance == null )
+        public static SingletonDoubleCheckLock GetInstance
+        {
+            // Now, imagine that the program has just been launched. 
+            // Since there's no Singleton instance yet, multiple threads can simultaneously pass the previous conditional and reach this point almost at the same time.
+            // The first of them will acquire lock and will proceed further, while the rest will wait here.
+            get
+            {
+                if ( _instance == null )
+                {
+                    lock ( _lock )
                     {
-                        _instance = new SingletonDoubleCheckLock();
-                        _instance.Value = value;
+                        // The first thread to acquire the lock, reaches this conditional, goes inside and creates the Singleton instance.
+                        // Once it leaves the lock block, a thread that might have been waiting for the lock release may then enter this section.
+                        // But since the Singleton field is already initialized, the thread won't create a new object.
+                        if ( _instance == null )
+                        {
+                            _instance = new SingletonDoubleCheckLock();
+                        }
                     }
                 }
+                return _instance;
             }
-
-            return _instance;
         }
 
-        public string Value { get; set; }
+        private SingletonDoubleCheckLock()
+        {
+            _counter++;
+            Console.WriteLine( $"Counter: { _counter }" );
+        }
+
+        public void PrintInformation( string message )
+        {
+            Console.WriteLine( message );
+        }
     }
 }
