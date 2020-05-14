ed8301592b61995af8de03da3451dc509987e794
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-14 14:40:59 +0200

    Add comment

--- DesignPattern/Singleton/ThreadSafeSingleton/SingletonEagerLoading.cs
+++ DesignPattern/Singleton/ThreadSafeSingleton/SingletonEagerLoading.cs
@@ -6,6 +6,7 @@ namespace Singleton.ThreadSafeSingleton
 {
     // The Eager loading in singleton design pattern is nothing a process in which we need to initialize the singleton object at the time of application start-up 
     // rather than on-demand and keep it ready in memory to be used in the future.
+
     // The advantage of using Eager Loading in the Singleton design pattern is that the CLR (Common Language Runtime) will take care of object initialization and thread-safety.
     // That means we will not require to write any code explicitly for handling the thread-safety for a multithreaded environment.
     public sealed class SingletonEagerLoading : ISingletonDemo
