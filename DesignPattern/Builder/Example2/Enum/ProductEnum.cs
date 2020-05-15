12dcd9090f605f48c082d32297d977762e7e72df
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-15 18:28:18 +0200

    Add Report enum

---
+++ DesignPattern/Builder/Example2/Enum/ProductEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Example2.Enum
+{
+    public enum ReportType
+    {
+        Excel,
+        PDF,
+        Word,
+        PowerPoint
+    }
+
+    public enum HeaderType
+    {
+        ExcelHeader,
+        PDFHeader,
+        WordHeader,
+        PowerPointHeader
+    }
+
+    public enum ContentType
+    {
+        ExcelContent,
+        PDFContent,
+        WordContent,
+        PowerPointContent
+    }
+
+    public enum FooterType
+    {
+        ExcelFooter,
+        PDFFooter,
+        WordFooter,
+        PowerPointFooter
+    }
+}
