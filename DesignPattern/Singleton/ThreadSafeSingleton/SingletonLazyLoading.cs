ed48cf0d8b929aed6ab5b63eb44ea4794b38e1bb
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-15 14:16:03 +0200

    Update comments

--- DesignPattern/Singleton/ThreadSafeSingleton/SingletonLazyLoading.cs
+++ DesignPattern/Singleton/ThreadSafeSingleton/SingletonLazyLoading.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Singleton.ThreadSafeSingleton
 {
     // The Lazy or Deferred Loading is a design pattern or you can say its a concept which is commonly used to delay the initialization of an object until the point at which it is needed.
-    // So the main objective of Lazy loading is to load the object on-demand or you can say object when needed.
+    // The main objective of Lazy loading is to load the object on-demand or you can say object when needed.
     // The most important point that you need to keep in mind is that, you need to use the Lazy loading when the cost of the object creation is very high as well as the use of that object is very rare.
     // The lazy loading improves the performance of an application if it is used properly. We can use the Lazy keyword to make the singleton instance as lazy loading. 
     public sealed class SingletonLazyLoading : ISingletonDemo
