80d884b222313898fd906ac9e750cb3544bee5d8
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-17 20:40:52 +0200

    Create class abstract for credit card factory

---
+++ DesignPattern/FactoryMethod/CreditCard/Factory/CreditCardFactory.cs
@@ -0,0 +1,17 @@
+﻿using FactoryMethod.CreditCard.Card;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod.CreditCard.Factory
+{
+    public abstract class CreditCardFactory
+    {
+        protected abstract ICreditCard MakeProduct();
+
+        public ICreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+}
