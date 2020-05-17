d1422c4266f83a647e1a1cc50f25f042e1ec936d
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-17 20:21:32 +0200

    Create 3 types of card: MoneyBack, Platium and Titanium

---
+++ DesignPattern/FactoryDesign/Card/MoneyBack.cs
@@ -0,0 +1,25 @@
+﻿using FactoryDesign.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryDesign.Card
+{
+    class MoneyBack : CreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 20000;
+        }
+
+        public CreditCardType GetCardType()
+        {
+            return CreditCardType.MoneyBack;
+        }
+
+        public int GetCreditLimit()
+        {
+            return 1000;
+        }
+    }
+}
