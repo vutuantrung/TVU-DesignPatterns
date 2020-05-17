fbcc7daa5a2fe98a6302752de1acea3e8c48ae51
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-17 20:49:36 +0200

    Update classes

---
+++ DesignPattern/FactoryAbstract/Products/ConcreteProductA2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryAbstract.Products
+{
+    public class ConcreteProductA2 : IAbstractProductA
+    {
+        public int GetProductNumber()
+        {
+            return 104;
+        }
+
+        public ProductTypes GetValue() => ProductTypes.ProductA2;
+    }
+}
---
+++ DesignPattern/UnitTests/AbstractFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using FactoryAbstract.Factory;
+using FactoryAbstract.Products;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class AbstractFactoryTests
+    {
+        [Test]
+        public void get_product_A()
+        {
+            {
+                IAbstractProductA prod = GetProduct( factory: new ConcreteFactory1() );
+
+                Assert.That( prod.GetValue() == ProductTypes.ProductA1 );
+            }
+
+            {
+                IAbstractProductA prod = GetProduct( factory: new ConcreteFactory2() );
+
+                Assert.That( prod.GetValue() == ProductTypes.ProductA2 );
+            }
+
+            IAbstractProductA GetProduct( IAbstractFactory factory ) => factory.CreateProductA();
+        }
+
+        [Test]
+        public void get_product_B()
+        {
+            {
+
+                IAbstractProductB prod = GetProduct( factory: new ConcreteFactory1() );
+
+                Assert.That( prod.GetValue() == ProductTypes.ProductB1 );
+            }
+
+            {
+                IAbstractProductB prod = GetProduct( factory: new ConcreteFactory2() );
+
+                Assert.That( prod.GetValue() == ProductTypes.ProductB2 );
+            }
+
+            IAbstractProductB GetProduct( IAbstractFactory factory ) => factory.CreateProductB();
+        }
+
+    }
+}
