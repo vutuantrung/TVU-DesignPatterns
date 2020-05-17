8367b10040b550db0125dda8782efd0c13ac1e1f
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-17 20:22:12 +0200

    Create credit card unittests

---
+++ DesignPattern/UnitTests/FactoryDesignTests.cs
@@ -0,0 +1,45 @@
+﻿using FactoryDesign.Card;
+using FactoryDesign.Enum;
+using FactoryDesign.Factory;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class FactoryDesignTests
+    {
+        [Test]
+        public void test_credit_card()
+        {
+            // MoneyBack card
+            {
+                CreditCard creditCard = CreditCardFactory.GetCreditCard( CreditCardType.MoneyBack );
+
+                Assert.That( creditCard.GetCardType() == CreditCardType.MoneyBack );
+                Assert.That( creditCard.GetAnnualCharge() == 20000 );
+                Assert.That( creditCard.GetCreditLimit() == 1000 );
+            }
+
+            // Platium card
+            {
+                CreditCard creditCard = CreditCardFactory.GetCreditCard( CreditCardType.Platium );
+
+                Assert.That( creditCard.GetCardType() == CreditCardType.Platium );
+                Assert.That( creditCard.GetAnnualCharge() == 30000 );
+                Assert.That( creditCard.GetCreditLimit() == 1500 );
+            }
+
+            // Titanium card
+            {
+                CreditCard creditCard = CreditCardFactory.GetCreditCard( CreditCardType.Titanium );
+
+                Assert.That( creditCard.GetCardType() == CreditCardType.Titanium );
+                Assert.That( creditCard.GetAnnualCharge() == 35000 );
+                Assert.That( creditCard.GetCreditLimit() == 2000 );
+            }
+        }
+    }
+}
