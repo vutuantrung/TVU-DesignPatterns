b5643f001d5dc5f12fecbbf838daee1ac3b8c6df
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-17 20:40:20 +0200

    Create 3 types of credit card: MoneyBack, Titanium and Platium

---
+++ DesignPattern/FactoryMethod/CreditCard/Card/Platium.cs
@@ -0,0 +1,25 @@
+﻿using FactoryMethod.CreditCard.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod.CreditCard.Card
+{
+    public class Platium : ICreditCard
+    {
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+
+        public CreditCardType GetCardType()
+        {
+            return CreditCardType.Platium;
+        }
+    }
+}
---
+++ DesignPattern/FactoryMethod/CreditCard/Card/Titanium.cs
@@ -0,0 +1,25 @@
+﻿using FactoryMethod.CreditCard.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod.CreditCard.Card
+{
+    class Titanium : ICreditCard
+    {
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+
+        public CreditCardType GetCardType()
+        {
+            return CreditCardType.Titanium;
+        }
+    }
+}
