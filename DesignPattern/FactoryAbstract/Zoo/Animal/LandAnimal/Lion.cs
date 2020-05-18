3942623023e1da1f0dbd32435aeeef7b3217df44
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-18 21:32:25 +0200

    Create land animals: Lion, Cat and Wolf

---
+++ DesignPattern/FactoryAbstract/Zoo/Animal/LandAnimal/Lion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Zoo.Animal.LandAnimal
+{
+    public class Lion : IAnimal
+    {
+        public string Speak()
+        {
+            return "Roar";
+        }
+    }
+}
---
+++ DesignPattern/FactoryAbstract/Zoo/Animal/LandAnimal/Woft.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Zoo.Animal.LandAnimal
+{
+    public class Woft : IAnimal
+    {
+        public string Speak()
+        {
+            return "Bark bark";
+        }
+    }
+}
