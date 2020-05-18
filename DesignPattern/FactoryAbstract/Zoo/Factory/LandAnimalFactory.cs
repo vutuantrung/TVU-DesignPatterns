78d09dac78cf2f63dc18372897b8c694abf9e07b
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-18 21:33:21 +0200

    Create land and see factory class

---
+++ DesignPattern/FactoryAbstract/Zoo/Factory/LandAnimalFactory.cs
@@ -0,0 +1,21 @@
+﻿using AbstractFactory.Zoo.Animal;
+using AbstractFactory.Zoo.Animal.LandAnimal;
+using AbstractFactory.Zoo.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Zoo.Factory
+{
+    class LandAnimalFactory : AnimalFactory
+    {
+        public override IAnimal GetAnimal( AnimalType animalType )
+        {
+            if ( animalType == AnimalType.Cat ) return new Cat();
+            if ( animalType == AnimalType.Wolf ) return new Woft();
+            if ( animalType == AnimalType.Lion ) return new Lion();
+
+            throw new ArgumentException( "Animal type do not exist.", nameof( animalType ) );
+        }
+    }
+}
