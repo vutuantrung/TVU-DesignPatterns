dbd851f52126e8fa00f8fccfd38d7c67ad6acb07
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-18 21:32:57 +0200

    Create enum animal type and animal factory type

---
+++ DesignPattern/FactoryAbstract/Zoo/Enum/AnimalType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Zoo.Enum
+{
+    public enum AnimalType
+    {
+        Wolf,
+        Lion,
+        Cat,
+        Shark,
+        Octopus
+    }
+
+    public enum AnimalFactoryType
+    {
+        Land,
+        Sea
+    }
+}
