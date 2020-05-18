ab96ff9bc1825ffddd71fab665d49fd1188bf8ca
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-18 21:32:41 +0200

    Create sea animals: Octopus and Shark

---
+++ DesignPattern/FactoryAbstract/Zoo/Animal/SeaAnimal/Octopus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Zoo.Animal.SeaAnimal
+{
+    public class Octopus : IAnimal
+    {
+        public string Speak()
+        {
+            return "SQUAWCK";
+        }
+    }
+}
---
+++ DesignPattern/FactoryAbstract/Zoo/Animal/SeaAnimal/Shark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Zoo.Animal.SeaAnimal
+{
+    public class Shark : IAnimal
+    {
+        public string Speak()
+        {
+            return "Cannot Speak";
+        }
+    }
+}
