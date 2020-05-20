5671db914e80acfa05351a3d9a899ebbfbec7c48
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-20 23:17:46 +0200

    Update Adapter design pattern project

---
+++ DesignPattern/StructuralDesignPattern/Adapter/EmployeeAdapter/Class/Employee.cs
@@ -0,0 +1,32 @@
+﻿using Adapter.EmployeeAdapter.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.EmployeeAdapter.Class
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Designation { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public PaySalary PaySalary { get; set; }
+
+        public double WorkingHours { get; set; }
+
+        public Employee( int id, string name, string designation, double workinghours, decimal salary )
+        {
+            ID = id;
+            Name = name;
+            Designation = designation;
+            Salary = salary;
+            PaySalary = PaySalary.NotPaid;
+            WorkingHours = workinghours;
+        }
+    }
+}
