3cb0dda1301139033b71dddee0eda475f4c367a9
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-21 00:17:31 +0200

    Add method to get information for unittest

--- DesignPattern/StructuralDesignPattern/Adapter/EmployeeAdapter/Adapter/ITarget.cs
+++ DesignPattern/StructuralDesignPattern/Adapter/EmployeeAdapter/Adapter/ITarget.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Adapter.EmployeeAdapter.Class;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,7 @@ namespace Adapter.EmployeeAdapter.Adapter
     public interface ITarget
     {
         void ProcessCompanySalary( string[,] employeesArray );
+
+        List<Employee> GetListEmployee();
     }
 }
