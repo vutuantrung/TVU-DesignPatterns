cfc1862269b8171af6c45e8befcf1d4c2566d79d
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-22 00:46:16 +0200

    Create shape interface

---
+++ DesignPattern/Facade/ShapeDraw/Class/IShape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade.ShapeDraw.Class
+{
+    public interface IShape
+    {
+        void Draw();
+        bool IsDrawn();
+    }
+}
