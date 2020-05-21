658bd7ce52d55a876ad9eff9d0e4e7673fcfa41b
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-21 20:03:11 +0200

    Add enum for nationality

---
+++ DesignPattern/StructuralDesignPattern/Adapter/SpeakerTranslatorAdapter/Enum/EnumNational.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.SpeakerTranslatorAdapter.Enum
+{
+    public enum Nationality
+    {
+        English,
+        French
+    }
+}
