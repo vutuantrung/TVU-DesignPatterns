862dc1406462d4df8e01eebaca4011a21886d7ce
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-22 00:58:17 +0200

    Create 2 subsystems

---
+++ DesignPattern/Facade/System/Class/SubSystem1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade.System.Class
+{
+    public class SubSystem1
+    {
+        public string Operation1()
+        {
+            return "Subsystem1: Ready!\n";
+        }
+
+        public string OperationN()
+        {
+            return "Subsystem1: Go!\n";
+        }
+    }
+}
