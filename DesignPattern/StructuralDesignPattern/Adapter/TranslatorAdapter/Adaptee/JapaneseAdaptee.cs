5493b8f4a4ba2943827d988ca8876b1861b41f92
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-21 18:37:25 +0200

    Create JP and FR adaptee

---
+++ DesignPattern/StructuralDesignPattern/Adapter/TranslatorAdapter/Adaptee/JapaneseAdaptee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.TranslatorAdapter.Adaptee
+{
+    public class JapaneseAdaptee
+    {
+        public void Receive( string word )
+        {
+            Console.WriteLine( "Retrieving words from Adapter ..." );
+            Console.WriteLine( word );
+        }
+    }
+}
