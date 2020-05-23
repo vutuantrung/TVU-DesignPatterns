31cb54a949cb7bcd71feec58008b7c002285c962
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-23 22:49:11 +0200

    Add abstract class car decorator

---
+++ DesignPattern/Decorator/Car/Decorator/CarDecorator.cs
@@ -0,0 +1,22 @@
+﻿using Decorator.Car.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Car.Decorator
+{
+    public abstract class CarDecorator : ICar
+    {
+        protected ICar _car;
+
+        public CarDecorator( ICar car )
+        {
+            _car = car;
+        }
+
+        public virtual ICar ManufactureCar()
+        {
+            return _car.ManufactureCar();
+        }
+    }
+}
