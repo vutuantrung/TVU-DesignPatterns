356e05d2e7524998f35f6264a1671183f46670a2
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-31 19:36:02 +0200

    Add TV remote classes and their abstract class

---
+++ DesignPattern/Bridge/TV/TVRemoteClass/RemoteControl.cs
@@ -0,0 +1,22 @@
+﻿using Bridge.TV;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.TVRemote
+{
+    public abstract class RemoteControl
+    {
+        protected ILEDTV _ledTV;
+
+        protected RemoteControl( ILEDTV ledTV )
+        {
+            _ledTV = ledTV;
+        }
+
+        public abstract void SwitchOn();
+        public abstract void SwitchOff();
+        public abstract void SetChannel( int channelNumber );
+
+    }
+}
