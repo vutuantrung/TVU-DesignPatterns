5189ce0f121be681d9d05a661243383b0acf353d
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-31 19:36:44 +0200

    Add TV unittests class and testing methods

---
+++ DesignPattern/StructuralDesignPattern/StructuralUnittests/BridgeTests.cs
@@ -0,0 +1,49 @@
+﻿using Bridge.Enum;
+using Bridge.TV;
+using Bridge.TVRemote;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralUnittests
+{
+    [TestFixture]
+    class BridgeTests
+    {
+        [Test]
+        public void Test_tv()
+        {
+            {
+                ILEDTV samsungTV = new SamsungLEDTV();
+
+                SamsungRemoteControl samsungRemote = new SamsungRemoteControl( samsungTV );
+                samsungRemote.SwitchOn();
+
+                Assert.That( samsungTV.GetPower() == PowerTV.On );
+
+                samsungRemote.SetChannel( 56 );
+
+                Assert.That( samsungTV.GetChannel() == 56 );
+
+                samsungRemote.SwitchOff();
+
+                Assert.That( samsungTV.GetPower() == PowerTV.Off );
+            }
+
+            {
+                ILEDTV sonyTV = new SonyLEDTV();
+
+                SamsungRemoteControl samsungRemote = new SamsungRemoteControl( sonyTV );
+                samsungRemote.SwitchOn();
+
+                Assert.That( sonyTV.GetPower() != PowerTV.On );
+
+                samsungRemote.SetChannel( 56 );
+
+                Assert.That( sonyTV.GetChannel() != 56 );
+
+            }
+        }
+    }
+}
