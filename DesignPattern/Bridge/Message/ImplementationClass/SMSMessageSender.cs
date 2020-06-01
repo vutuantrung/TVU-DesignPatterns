adc951baa8e8ffff9970aa96e2d1082124784911
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-02 01:57:15 +0200

    Create message sender and their interface

---
+++ DesignPattern/Bridge/Message/ImplementationClass/SMSMessageSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Message.ImplementationClass
+{
+    public class SMSMessageSender : IMessageSender
+    {
+        public bool SendMessage( string message )
+        {
+            return true;
+        }
+    }
+}
