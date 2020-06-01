168018a09d31eb1eb4d99784717d62a8acda4051
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-02 00:29:08 +0200

    Create implementations and their interface

---
+++ DesignPattern/Bridge/Demo/ImplementationClass/ConcreteImplementationA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Demo.ImplementationClass
+{
+    public class ConcreteImplementationA : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ImplementationA";
+        }
+    }
+}
---
+++ DesignPattern/Bridge/Demo/ImplementationClass/ConcreteImplementationB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Demo.ImplementationClass
+{
+    public class ConcreteImplementationB : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ImplementationB";
+        }
+    }
+}
