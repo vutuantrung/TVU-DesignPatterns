d9b6d187863ede80f3c82b4c77e67b8939396841
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-02 00:28:53 +0200

    Create abstraction class

---
+++ DesignPattern/Bridge/Demo/AbstractionClass/Abstraction.cs
@@ -0,0 +1,29 @@
+﻿using Bridge.Demo.ImplementationClass;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Demo.AbstractionClass
+{
+    public class Abstraction
+    {
+        protected IImplementation _implementation;
+
+        string _data;
+
+        public Abstraction( IImplementation implementation )
+        {
+            _implementation = implementation;
+        }
+
+        public virtual void Operation()
+        {
+            if ( _implementation == null ) throw new ArgumentNullException( nameof( _implementation ) );
+
+            _data = _implementation.OperationImplementation();
+
+        }
+
+        public string GetData() => _data;
+    }
+}
