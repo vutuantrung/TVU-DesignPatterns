2ee07b18b9e3de6fb500ab62a2dce9a42cc274bc
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-02 22:32:59 +0200

    Create composite class

---
+++ DesignPattern/Composite/Computer/Class/DeviceComposite.cs
@@ -0,0 +1,42 @@
+﻿using Composite.Computer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite.Computer.Class
+{
+    public class DeviceComposite : IDeviceComponent
+    {
+        private DeviceType _type;
+
+        List<IDeviceComponent> _components;
+
+        public DeviceComposite( DeviceType type )
+        {
+            _type = type;
+            _components = new List<IDeviceComponent>();
+        }
+
+        public void AddComponent( IDeviceComponent component )
+        {
+            _components.Add( component );
+        }
+
+        public DeviceType GetDeviceType()
+        {
+            return _type;
+        }
+
+        public int GetPrice()
+        {
+            int totalPrice = 0;
+
+            foreach ( IDeviceComponent component in _components )
+            {
+                totalPrice += component.GetPrice();
+            }
+
+            return totalPrice;
+        }
+    }
+}
