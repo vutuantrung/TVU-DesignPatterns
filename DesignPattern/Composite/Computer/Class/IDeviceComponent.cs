41730bc9d7ccda115db6aef95945ddeadf0f8c52
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-02 22:33:26 +0200

    Create component interface

---
+++ DesignPattern/Composite/Computer/Class/IDeviceComponent.cs
@@ -0,0 +1,13 @@
+﻿using Composite.Computer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite.Computer
+{
+    public interface IDeviceComponent
+    {
+        DeviceType GetDeviceType();
+        int GetPrice();
+    }
+}
