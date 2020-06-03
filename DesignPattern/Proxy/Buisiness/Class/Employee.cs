638c86d08a1cf2fd3cf341f2ae9f0064dcd0221d
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-03 20:55:21 +0200

    Add classes object

---
+++ DesignPattern/Proxy/Buisiness/Class/Employee.cs
@@ -0,0 +1,35 @@
+﻿using Proxy.Buisiness.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.Buisiness.Class
+{
+    public class Employee
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public EmployeeType Role { get; set; }
+
+        private Package _package;
+
+        public Employee( string username, string password, EmployeeType role )
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+
+        public void ReceiveSharedFolder( ISharedFolder sharedFolder )
+        {
+            _package = sharedFolder.GetPackage();
+        }
+
+        public Package GetPackage()
+        {
+            return _package;
+        }
+    }
+}
