7ebb18fc2669335db649868c2d633735ff3475eb
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-03 20:56:34 +0200

    Create Proxy unittests and add Business unittests

---
+++ DesignPattern/StructuralDesignPattern/StructuralUnittests/Proxy.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using Proxy.Buisiness.Class;
+using Proxy.Buisiness.Enum;
+using Proxy.Buisiness.Proxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralUnittests
+{
+    [TestFixture]
+    class Proxy
+    {
+        [Test]
+        public void Test_business()
+        {
+            {
+                Employee person = new Employee( "userTest", "password", EmployeeType.Manager );
+                SharedFolderProxy proxy = new SharedFolderProxy( person );
+                proxy.SetAccessingPackageLevel();
+                proxy.PerformOperation();
+
+                Assert.That( person.Username, Is.EqualTo( "userTest" ) );
+                Assert.That( person.Password, Is.EqualTo( "password" ) );
+                Assert.That( person.GetPackage().CanRead && person.GetPackage().CanWrite );
+            }
+
+            {
+                Employee person = new Employee( "userTest", "password", EmployeeType.CEO );
+                SharedFolderProxy proxy = new SharedFolderProxy( person );
+                proxy.SetAccessingPackageLevel();
+                proxy.PerformOperation();
+
+                Assert.That( person.Username, Is.EqualTo( "userTest" ) );
+                Assert.That( person.Password, Is.EqualTo( "password" ) );
+                Assert.That( person.GetPackage().CanRead && person.GetPackage().CanWrite );
+            }
+
+            {
+                Employee person = new Employee( "userTest", "password", EmployeeType.Developer );
+                SharedFolderProxy proxy = new SharedFolderProxy( person );
+                proxy.SetAccessingPackageLevel();
+                proxy.PerformOperation();
+
+                Assert.That( person.Username, Is.EqualTo( "userTest" ) );
+                Assert.That( person.Password, Is.EqualTo( "password" ) );
+                Assert.That( person.GetPackage().CanRead && person.GetPackage().CanWrite );
+            }
+
+            {
+                Employee person = new Employee( "userTest", "password", EmployeeType.Staff );
+                SharedFolderProxy proxy = new SharedFolderProxy( person );
+                proxy.SetAccessingPackageLevel();
+                proxy.PerformOperation();
+
+                Assert.That( person.Username, Is.EqualTo( "userTest" ) );
+                Assert.That( person.Password, Is.EqualTo( "password" ) );
+                Assert.That( person.GetPackage().CanRead );
+            }
+
+            {
+                Employee person = new Employee( "userTest", "password", EmployeeType.Visiter );
+                SharedFolderProxy proxy = new SharedFolderProxy( person );
+                proxy.SetAccessingPackageLevel();
+                proxy.PerformOperation();
+
+                Assert.That( person.Username, Is.EqualTo( "userTest" ) );
+                Assert.That( person.Password, Is.EqualTo( "password" ) );
+                Assert.That( !person.GetPackage().CanRead  );
+            }
+        }
+    }
+}
