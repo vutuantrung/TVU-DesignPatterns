56ecc78f263729850f014a86ef73cfb5649c9428
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-05 00:15:28 +0200

    Create shapes and their interface

---
+++ DesignPattern/Flyweight/Shape/Class/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using Flyweight.Shape.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight.Shape.Class
+{
+    public class Rectangle : IShape
+    {
+        public ShapeColor Color { get; set; }
+
+        public void SetColor( ShapeColor color )
+        {
+            Color = color;
+        }
+
+        public IShape Draw()
+        {
+            return this;
+        }
+    }
+}
