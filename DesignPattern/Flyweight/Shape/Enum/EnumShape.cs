fba555637cf997b1aa75121c22da0c30fdcc019c
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-05 00:15:44 +0200

    Create enum shape

---
+++ DesignPattern/Flyweight/Shape/Enum/EnumShape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight.Shape.Enum
+{
+    public enum ShapeColor
+    {
+        White,
+        Black,
+        Pink,
+        Orange
+    }
+
+    public enum ShapeType
+    {
+        Circle,
+        Rectangle,
+        Triangle,
+        Square
+    }
+}
