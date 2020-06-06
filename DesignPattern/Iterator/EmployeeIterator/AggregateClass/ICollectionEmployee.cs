c77a8685bb43a39f4c098ca0baefce9d672476f1
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-07 01:32:25 +0200

    Create collection list, collection array and their interface

---
+++ DesignPattern/Iterator/EmployeeIterator/AggregateClass/ICollectionEmployee.cs
@@ -0,0 +1,16 @@
+﻿using Iterator.EmployeeIterator.Class;
+using Iterator.EmployeeIterator.IteratorClass;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator.EmployeeIterator.AggregateClass
+{
+    public interface ICollectionEmployee
+    {
+        IteratorEmployee CreateIterator();
+        Employee GetEmployee( int indexPosition );
+        void AddEmployee( Employee employee );
+        int Count();
+    }
+}
