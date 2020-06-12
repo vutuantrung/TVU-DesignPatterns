a2ac9fb125b1d53faa79509041749d622e1db9a6
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-12 20:25:21 +0200

    Create Observer unittests

---
+++ DesignPattern/BehavioralUnittests/Observer.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehavioralUnittests
+{
+    [TestFixture]
+    class Observer
+    {
+        [Test]
+        public void Test_mobile_phone()
+        {
+
+        }
+    }
+}
