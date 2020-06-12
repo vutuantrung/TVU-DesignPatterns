9f18153bfb4c98a7f576a19b6f298f3f80d31a97
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-12 20:58:10 +0200

    Create Enums

---
+++ DesignPattern/Observer/Product/Enum/ObserverEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer.Product.Enum
+{
+    public enum ObserverType
+    {
+        Promotion,
+        Availability
+    }
+}
