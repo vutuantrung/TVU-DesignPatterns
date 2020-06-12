5f8a16d3df818c9fc80b2b3d83d512b1cdf54c79
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-12 20:58:58 +0200

    Add unittests for Product

--- DesignPattern/BehavioralUnittests/Observer.cs
+++ DesignPattern/BehavioralUnittests/Observer.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using Observer.Product.Enum;
+using Observer.Product.ProductObserver;
+using Observer.Product.Subject;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +12,34 @@ namespace BehavioralUnittests
     class Observer
     {
         [Test]
-        public void Test_mobile_phone()
+        public void Test_product()
         {
+            var mobileProduct = new Subject( ProductType.MobilePhone, 565.50, Availability.OutOfStock );
+            var latopProduct = new Subject( ProductType.Laptop, 1250.30, Availability.OutOfStock );
 
+            var client1 = new ProductObserver( "Anurag", mobileProduct );
+            var client2 = new ProductObserver( "Pranaya", mobileProduct );
+            var client3 = new ProductObserver( "Priyanka", mobileProduct );
+
+            mobileProduct.SetAvailability( Availability.Available );
+
+            Assert.That( client1.MessageReceived == "We are happy to inform you that your product is now avaible." );
+            Assert.That( client2.MessageReceived == "We are happy to inform you that your product is now avaible." );
+            Assert.That( client3.MessageReceived == "We are happy to inform you that your product is now avaible." );
+
+            mobileProduct.SetAvailability( Availability.OutOfStock );
+
+            Assert.That( client1.MessageReceived == "We are very sorry to inform you that our product is now unavaible." );
+            Assert.That( client2.MessageReceived == "We are very sorry to inform you that our product is now unavaible." );
+            Assert.That( client3.MessageReceived == "We are very sorry to inform you that our product is now unavaible." );
+
+            var client4 = new ProductObserver( "Torung", latopProduct );
+            var client5 = new ProductObserver( "John", latopProduct );
+
+            latopProduct.SetPromotion( 200.00 );
+
+            Assert.That( client4.MessageReceived == $"Ours products is now in promotion with a new price { latopProduct.ProductPrice }" );
+            Assert.That( client5.MessageReceived == $"Ours products is now in promotion with a new price { latopProduct.ProductPrice }" );
         }
     }
 }
