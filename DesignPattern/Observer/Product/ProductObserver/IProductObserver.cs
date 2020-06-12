208e204bad6379824a8f52247e0ffd13332efb75
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-12 20:58:21 +0200

    Create observer and its interface

---
+++ DesignPattern/Observer/Product/ProductObserver/IProductObserver.cs
@@ -0,0 +1,13 @@
+﻿using Observer.Product.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer.Product.ProductObserver
+{
+    public interface IProductObserver
+    {
+        void UpdateAvailability( Availability availability );
+        void UpdatePromotion( double promotionPrice );
+    }
+}
