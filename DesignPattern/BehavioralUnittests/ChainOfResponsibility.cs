d552d6b265a9fc9d7f6765ecc6f5ec6da33953ad
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-13 21:54:32 +0200

    Add unittests for CustomATM

--- DesignPattern/BehavioralUnittests/ChainOfResponsibility.cs
+++ DesignPattern/BehavioralUnittests/ChainOfResponsibility.cs
@@ -21,6 +21,7 @@ namespace BehavioralUnittests
                 var moneyNoteList = atm.GetListMoneyNote();
 
                 Assert.That( moneyNoteList.GetMoneyNoteList().Count, Is.EqualTo( 7 ) );
+                Assert.That( moneyNoteList.GetNumberNote(), Is.EqualTo( 13 ) );
 
                 var collection = atm.GetListMoneyNote();
 
@@ -51,6 +52,7 @@ namespace BehavioralUnittests
                 var moneyNoteList = atm.GetListMoneyNote();
 
                 Assert.That( moneyNoteList.GetMoneyNoteList().Count, Is.EqualTo( 7 ) );
+                Assert.That( moneyNoteList.GetNumberNote(), Is.EqualTo( 18 ) );
 
                 var collection = atm.GetListMoneyNote();
 
@@ -81,6 +83,106 @@ namespace BehavioralUnittests
                 var moneyNoteList = atm.GetListMoneyNote();
 
                 Assert.That( moneyNoteList.GetMoneyNoteList().Count, Is.EqualTo( 7 ) );
+                Assert.That( moneyNoteList.GetNumberNote(), Is.EqualTo( 21 ) );
+
+                var collection = atm.GetListMoneyNote();
+
+                MoneyNote twoThousand = collection.GetMoneyNote( MoneyNoteDenomination.TwoThousand );
+                Assert.That( twoThousand.Count, Is.EqualTo( 6 ) );
+
+                MoneyNote oneThousand = collection.GetMoneyNote( MoneyNoteDenomination.OneThousand );
+                Assert.That( oneThousand.Count, Is.EqualTo( 0 ) );
+
+                MoneyNote fiveHundred = collection.GetMoneyNote( MoneyNoteDenomination.FiveHundred );
+                Assert.That( fiveHundred.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote oneHundred = collection.GetMoneyNote( MoneyNoteDenomination.OneHundred );
+                Assert.That( oneHundred.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote fifty = collection.GetMoneyNote( MoneyNoteDenomination.Fifty );
+                Assert.That( fifty.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote ten = collection.GetMoneyNote( MoneyNoteDenomination.Ten );
+                Assert.That( ten.Count, Is.EqualTo( 4 ) );
+
+                MoneyNote one = collection.GetMoneyNote( MoneyNoteDenomination.One );
+                Assert.That( one.Count, Is.EqualTo( 8 ) );
+            }
+        }
+
+        [Test]
+        public void Test_ATM_money_handler()
+        {
+            CustomATM atm = new CustomATM();
+
+            {
+                atm.WithDrawMoney( 2569.00 );
+                var moneyNoteList = atm.GetListMoneyNote();
+
+                Assert.That( moneyNoteList.GetMoneyNoteList().Count, Is.EqualTo( 7 ) );
+                Assert.That( moneyNoteList.GetNumberNote(), Is.EqualTo( 13 ) );
+
+                var collection = atm.GetListMoneyNote();
+
+                MoneyNote twoThousand = collection.GetMoneyNote( MoneyNoteDenomination.TwoThousand );
+                Assert.That( twoThousand.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote oneThousand = collection.GetMoneyNote( MoneyNoteDenomination.OneThousand );
+                Assert.That( oneThousand.Count, Is.EqualTo( 0 ) );
+
+                MoneyNote fiveHundred = collection.GetMoneyNote( MoneyNoteDenomination.FiveHundred );
+                Assert.That( fiveHundred.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote oneHundred = collection.GetMoneyNote( MoneyNoteDenomination.OneHundred );
+                Assert.That( oneHundred.Count, Is.EqualTo( 0 ) );
+
+                MoneyNote fifty = collection.GetMoneyNote( MoneyNoteDenomination.Fifty );
+                Assert.That( fifty.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote ten = collection.GetMoneyNote( MoneyNoteDenomination.Ten );
+                Assert.That( ten.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote one = collection.GetMoneyNote( MoneyNoteDenomination.One );
+                Assert.That( one.Count, Is.EqualTo( 9 ) );
+            }
+
+            {
+                atm.WithDrawMoney( 5946 );
+                var moneyNoteList = atm.GetListMoneyNote();
+
+                Assert.That( moneyNoteList.GetMoneyNoteList().Count, Is.EqualTo( 7 ) );
+                Assert.That( moneyNoteList.GetNumberNote(), Is.EqualTo( 18 ) );
+
+                var collection = atm.GetListMoneyNote();
+
+                MoneyNote twoThousand = collection.GetMoneyNote( MoneyNoteDenomination.TwoThousand );
+                Assert.That( twoThousand.Count, Is.EqualTo( 2 ) );
+
+                MoneyNote oneThousand = collection.GetMoneyNote( MoneyNoteDenomination.OneThousand );
+                Assert.That( oneThousand.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote fiveHundred = collection.GetMoneyNote( MoneyNoteDenomination.FiveHundred );
+                Assert.That( fiveHundred.Count, Is.EqualTo( 1 ) );
+
+                MoneyNote oneHundred = collection.GetMoneyNote( MoneyNoteDenomination.OneHundred );
+                Assert.That( oneHundred.Count, Is.EqualTo( 4 ) );
+
+                MoneyNote fifty = collection.GetMoneyNote( MoneyNoteDenomination.Fifty );
+                Assert.That( fifty.Count, Is.EqualTo( 0 ) );
+
+                MoneyNote ten = collection.GetMoneyNote( MoneyNoteDenomination.Ten );
+                Assert.That( ten.Count, Is.EqualTo( 4 ) );
+
+                MoneyNote one = collection.GetMoneyNote( MoneyNoteDenomination.One );
+                Assert.That( one.Count, Is.EqualTo( 6 ) );
+            }
+
+            {
+                atm.WithDrawMoney( 12698 );
+                var moneyNoteList = atm.GetListMoneyNote();
+
+                Assert.That( moneyNoteList.GetMoneyNoteList().Count, Is.EqualTo( 7 ) );
+                Assert.That( moneyNoteList.GetNumberNote(), Is.EqualTo( 21 ) );
 
                 var collection = atm.GetListMoneyNote();
 
