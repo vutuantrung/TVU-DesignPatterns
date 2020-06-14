9aecf05936e4241790b8562db358a223e3190b46
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-14 17:37:31 +0200

    Create handler classes and their abstract class

---
+++ DesignPattern/ChainOfResponsibility/Reporting/ReportingHandler/Employee.cs
@@ -0,0 +1,19 @@
+﻿using ChainOfResponsibility.Reporting.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Reporting.ReportingHandler
+{
+    public abstract class Employee
+    {
+        protected Employee _supervisor;
+
+        public void SetNextSupervisor( Employee supervisor )
+        {
+            _supervisor = supervisor;
+        }
+
+        public abstract void ApplyLeave( LeavingRecording record );
+    }
+}
---
+++ DesignPattern/ChainOfResponsibility/Reporting/ReportingHandler/HR.cs
@@ -0,0 +1,37 @@
+﻿using ChainOfResponsibility.Reporting.Class;
+using ChainOfResponsibility.Reporting.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Reporting.ReportingHandler
+{
+    public class HR : Employee
+    {
+        private int MAX_LEAVES_CAN_APPROVE = 30;
+
+        public override void ApplyLeave( LeavingRecording record )
+        {
+            if ( record.NumberOfDaysLeave <= MAX_LEAVES_CAN_APPROVE )
+            {
+                ApproveLeave( record );
+            }
+            else
+            {
+                Decline( record );
+            }
+        }
+
+        private void Decline( LeavingRecording record )
+        {
+            record.IsApproved = false;
+            record.SectionDecided = SectionType.HR;
+        }
+
+        private void ApproveLeave( LeavingRecording record )
+        {
+            record.IsApproved = true;
+            record.SectionDecided = SectionType.HR;
+        }
+    }
+}
