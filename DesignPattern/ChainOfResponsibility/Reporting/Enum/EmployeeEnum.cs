a18e633768a79bfb58182d5c9aa010efeab2827e
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-14 17:37:42 +0200

    Create enum class

---
+++ DesignPattern/ChainOfResponsibility/Reporting/Enum/EmployeeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Reporting.Enum
+{
+    public enum SectionType
+    {
+        Developer,
+        TeamLeader,
+        ProjectLeader,
+        HR
+    }
+}
