1a002ac2a2a2fe07e8513c89928e7694bb24a6d5
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-15 21:31:21 +0200

    Add main classes

---
+++ DesignPattern/State/BankCard/Class/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State.BankCard.Class
+{
+    public class Card
+    {
+        public string CardNumber { get; set; }
+
+        public string OnwerName { get; set; }
+    }
+}
