7c56426bf44ea82555dbddcc5aa891b574a88f42
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-15 21:31:58 +0200

    Add unittests for BankCard

--- DesignPattern/BehavioralUnittests/State.cs
+++ DesignPattern/BehavioralUnittests/State.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using State.BankCard.Class;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,43 @@ namespace BehavioralUnittests
     [TestFixture]
     class State
     {
+        [Test]
+        public void Test_bank_card()
+        {
+            Server server = new Server.Builder().SetCardNumberDB( "9856 4587 5656 2325" )
+                                                .SetOwnerNameDB( "John Smith" )
+                                                .SetMoneyDB( 100000 )
+                                                .SetPinDB( "6981" )
+                                                .Build();
+
+            ATMMachine machine = new ATMMachine( server );
+
+            Card cardA = new Card { CardNumber = "9856 4587 5656 2325", OnwerName = "John Smith" };
+            Card cardB = new Card { CardNumber = "9476 4587 1666 2345", OnwerName = "John Smith" };
+            Card cardC = new Card { CardNumber = "9856 4587 5656 2325", OnwerName = "Mary Smith" };
+
+            {
+                machine.InsertDebitCard( card: cardA );
+
+                Assert.That( machine.EnterPin( "6981" ), Is.EqualTo( true ) );
+                Assert.That( machine.WithdrawMoney( 5000 ), Is.EqualTo( true ) );
+                Assert.That( machine.WithdrawMoney( 200000 ), Is.EqualTo( false ) );
+
+                Assert.Throws<InvalidOperationException>( () => machine.InsertDebitCard( card: cardA ), "Card already in machine, can not insert more than 1 card." );
+
+                machine.EjectDebitCard();
+
+                Assert.Throws<InvalidOperationException>( () => machine.EnterPin( "15354" ), "Card not found, can not enter pin." );
+                Assert.Throws<InvalidOperationException>( () => machine.EjectDebitCard(), "Card not found, can not eject card." );
+            }
+
+            {
+                Assert.Throws<ArgumentException>( () => machine.InsertDebitCard( card: cardB ), "Card with this number does not exist in DB." );
+            }
+
+            {
+                Assert.Throws<ArgumentException>( () => machine.InsertDebitCard( card: cardC ), "Card with this name does not exist in DB." );
+            }
+        }
     }
 }
