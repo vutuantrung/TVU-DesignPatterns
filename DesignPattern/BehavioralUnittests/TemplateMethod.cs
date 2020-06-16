fe710f678f4ddd54fe50338191348aaa67711291
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-16 21:52:44 +0200

    Add unittests for BuildHouse

--- DesignPattern/BehavioralUnittests/TemplateMethod.cs
+++ DesignPattern/BehavioralUnittests/TemplateMethod.cs
@@ -2,11 +2,39 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TemplateMethod.BuildHouse.Class;
+using TemplateMethod.BuildHouse.Enum;
+using TemplateMethod.BuildHouse.Template;
 
 namespace BehavioralUnittests
 {
     [TestFixture]
     public class TemplateMethod
     {
+        [Test]
+        public void Test_house()
+        {
+            {
+                House house = new House();
+                HouseTemplate template = new NormalHouseBuild( house );
+                template.BuildHouse();
+
+                Assert.That( house.Foundation, Is.EqualTo( FoundationMaterial.Normal ) );
+                Assert.That( house.Wall, Is.EqualTo( WallMaterial.Normal ) );
+                Assert.That( house.Pillars, Is.EqualTo( PillarMaterial.Normal ) );
+                Assert.That( house.Window, Is.EqualTo( WindowMaterial.Normal ) );
+            }
+
+            {
+                House house = new House();
+                HouseTemplate template = new WoodHouseBuild( house );
+                template.BuildHouse();
+
+                Assert.That( house.Foundation, Is.EqualTo( FoundationMaterial.Wood ) );
+                Assert.That( house.Wall, Is.EqualTo( WallMaterial.Wood ) );
+                Assert.That( house.Pillars, Is.EqualTo( PillarMaterial.Wood ) );
+                Assert.That( house.Window, Is.EqualTo( WindowMaterial.Wood ) );
+            }
+        }
     }
 }
