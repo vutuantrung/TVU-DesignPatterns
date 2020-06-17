9635ae2bb67c09880d797379f0f176345a7c3ba8
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-17 21:29:26 +0200

    Add invoker class

---
+++ DesignPattern/Command/Document/Invoker/MenuOption.cs
@@ -0,0 +1,36 @@
+﻿using Command.Document.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command.Document.Invoker
+{
+    public class MenuOption
+    {
+        private ICommand _openCommand;
+        private ICommand _saveCommand;
+        private ICommand _closeCommand;
+
+        public MenuOption( ICommand openCommand, ICommand saveCommand, ICommand closeCommand )
+        {
+            _openCommand = openCommand;
+            _saveCommand = saveCommand;
+            _closeCommand = closeCommand;
+        }
+
+        public void OpenClick()
+        {
+            _openCommand.Execute();
+        }
+
+        public void SaveClick()
+        {
+            _saveCommand.Execute();
+        }
+
+        public void CloseClick()
+        {
+            _closeCommand.Execute();
+        }
+    }
+}
