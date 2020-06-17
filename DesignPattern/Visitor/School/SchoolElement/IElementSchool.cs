c8710b0cb9d2865c6afcbbce954938b7d4763582
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-17 23:44:48 +0200

    Add element classes

---
+++ DesignPattern/Visitor/School/SchoolElement/IElementSchool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Visitor.School.Enum;
+using Visitor.School.SchoolVisitor;
+
+namespace Visitor.School.SchoolElement
+{
+    public interface IElementSchool
+    {
+        void Accept( IVisitorSchool visitor );
+        void GivingMedication( MedicineType medicineType );
+        void GivingGift( GiftType giftType );
+        MedicineType GetMedicine();
+        GiftType GetGift();
+    }
+}
---
+++ DesignPattern/Visitor/School/SchoolElement/Teacher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Visitor.School.Enum;
+using Visitor.School.SchoolVisitor;
+
+namespace Visitor.School.SchoolElement
+{
+    public class Teacher : IElementSchool
+    {
+        public string Name { get; set; }
+
+        private MedicineType _medicineType;
+        private GiftType _giftType;
+
+        public Teacher( string name )
+        {
+            Name = name;
+        }
+
+        public void Accept( IVisitorSchool visitor )
+        {
+            visitor.Visit( this );
+        }
+
+        public MedicineType GetMedicine()
+        {
+            return _medicineType;
+        }
+
+        public GiftType GetGift()
+        {
+            return _giftType;
+        }
+
+        public void GivingMedication( MedicineType medicineType )
+        {
+            _medicineType = medicineType;
+        }
+
+        public void GivingGift( GiftType giftType )
+        {
+            _giftType = giftType;
+        }
+    }
+}
