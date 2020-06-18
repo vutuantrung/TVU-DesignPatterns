1bfca4cfb3041c3e3b2540a2fdbdb869794380de
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-19 01:32:53 +0200

    Add expression classes

---
+++ DesignPattern/Interpreter/DateTimeConvert/Class/IExpression.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter.DateTimeConvert.Class
+{
+    public interface IExpression
+    {
+        void Evaluate( Context context );
+    }
+}
