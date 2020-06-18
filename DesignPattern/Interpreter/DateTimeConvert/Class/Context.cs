a4793f587de4e6c7af2daff55637273b2d8c5e7f
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-19 01:33:02 +0200

    Add context class

---
+++ DesignPattern/Interpreter/DateTimeConvert/Class/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter.DateTimeConvert.Class
+{
+    public class Context
+    {
+        public string Expression { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Context( DateTime dateTime )
+        {
+            this.DateTime = dateTime;
+        }
+    }
+}
