a22b3c2421448254578a0e306a8393f6ae4bb112
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-18 21:59:13 +0200

    Add recording class and context class

---
+++ DesignPattern/Strategy/Compression/Class/CompressionContext.cs
@@ -0,0 +1,28 @@
+﻿using Strategy.Compression.StrategyCompression;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Compression.Class
+{
+    // This is a class which maintains a reference to a Strategy object, and then uses that reference to call the algorithm defined by a particular ConcreteStrategy
+    public class CompressionContext
+    {
+        ICompression _compression;
+
+        public CompressionContext( ICompression compression )
+        {
+            _compression = compression;
+        }
+
+        public void SetStrategy( ICompression compression )
+        {
+            _compression = compression;
+        }
+
+        public void CreateArchive( File file )
+        {
+            _compression.CompressFolder( file );
+        }
+    }
+}
---
+++ DesignPattern/Strategy/Compression/Class/File.cs
@@ -0,0 +1,14 @@
+﻿using Strategy.Compression.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Compression.Class
+{
+    public class File
+    {
+        public CompressionType Comporession { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
