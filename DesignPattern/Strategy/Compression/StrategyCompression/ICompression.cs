c9c8e866f0b86fb8592c8ba15f7cc861ac579e5f
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-18 21:58:52 +0200

    Add strategy classes

---
+++ DesignPattern/Strategy/Compression/StrategyCompression/ICompression.cs
@@ -0,0 +1,13 @@
+﻿using Strategy.Compression.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Compression.StrategyCompression
+{
+    // The Strategy declares an interface that is going to be implemented by all supported algorithms.
+    public interface ICompression
+    {
+        void CompressFolder( File file );
+    }
+}
