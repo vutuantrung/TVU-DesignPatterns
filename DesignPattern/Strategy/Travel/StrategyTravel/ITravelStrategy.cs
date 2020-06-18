bb35d4516ed8c37db970ec18d532c677a6787c51
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-18 23:43:25 +0200

    Add strategy classes

---
+++ DesignPattern/Strategy/Travel/StrategyTravel/ITravelStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Travel.StrategyTravel
+{
+    public interface ITravelStrategy
+    {
+        void ToTheAirport();
+    }
+}
