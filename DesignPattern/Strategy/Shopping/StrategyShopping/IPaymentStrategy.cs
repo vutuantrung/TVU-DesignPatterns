6935e8320a7320347ef7229a21a45e29bda135d6
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-18 23:57:54 +0200

    Add strategy classes

---
+++ DesignPattern/Strategy/Shopping/StrategyShopping/IPaymentStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Shopping.StrategyShopping
+{
+    public interface IPaymentStrategy
+    {
+        void Pay( double amount );
+    }
+}
