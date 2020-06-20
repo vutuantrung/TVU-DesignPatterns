f16251bf535abffdb425d897e09d7a6844aacc46
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-21 01:38:04 +0200

    Add component classes

---
+++ DesignPattern/Mediator/Demo/Class/BaseComponent.cs
@@ -0,0 +1,27 @@
+﻿using Mediator.Demo.MediatorDemo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator.Demo.Class
+{
+    public abstract class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent( IMediator mediator = null )
+        {
+            _mediator = mediator;
+        }
+
+        public void SetMediator( IMediator mediator )
+        {
+            _mediator = mediator;
+        }
+
+        public void ReceiveMessage( string message )
+        {
+            Console.WriteLine( message );
+        }
+    }
+}
---
+++ DesignPattern/Mediator/Demo/Class/ConcreteComponentB.cs
@@ -0,0 +1,27 @@
+﻿using Mediator.Demo.MediatorDemo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator.Demo.Class
+{
+    public class ConcreteComponentB : BaseComponent
+    {
+        public ConcreteComponentB( IMediator mediator = null )
+            : base( mediator )
+        {
+        }
+
+        public void ExecutingA()
+        {
+            if ( _mediator == null ) throw new ArgumentNullException( nameof( _mediator ) );
+            _mediator.Notify( this, "B do A" );
+        }
+
+        public void ExecutingB()
+        {
+            if ( _mediator == null ) throw new ArgumentNullException( nameof( _mediator ) );
+            _mediator.Notify( this, "B do B" );
+        }
+    }
+}
