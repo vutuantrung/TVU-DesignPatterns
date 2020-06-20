6a0a4505cb193362b7866710bce2b65629418708
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-20 16:06:34 +0200

    Add main classes

---
+++ DesignPattern/Mediator/ChattingGroup/Class/User.cs
@@ -0,0 +1,28 @@
+﻿using Mediator.ChattingGroup.MediatorFacebook;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator.ChattingGroup.Class
+{
+    public abstract class UserAbstract
+    {
+        protected IChattingGroupMediator _mediatorFacebook;
+        protected IChattingGroupMediator _mediatorInstagram;
+
+        protected string _name;
+
+        public UserAbstract( string name )
+        {
+            _name = name;
+        }
+
+        public abstract void SendMessageToFacebook( string message );
+        public abstract void ReceiveFromFacebook( string message );
+        public abstract void SendMessageToInstagram( string message );
+        public abstract void ReceiveFromInstagram( string message );
+
+        public abstract void RegisterFacebookMediator( IChattingGroupMediator mediator );
+        public abstract void RegisterInstagramMediator( IChattingGroupMediator mediator );
+    }
+}
