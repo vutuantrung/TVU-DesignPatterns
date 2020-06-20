b58efe74a4ff409a97b72f8f90ee58f64bbc32e7
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-20 16:05:14 +0200

    Add mediator classes

---
+++ DesignPattern/Mediator/ChattingGroup/MediatorFacebook/InstagramGroupMediator.cs
@@ -0,0 +1,34 @@
+﻿using Mediator.ChattingGroup.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator.ChattingGroup.MediatorFacebook
+{
+    public class InstagramGroupMediator : IChattingGroupMediator
+    {
+        private List<UserAbstract> _listUser;
+
+        public InstagramGroupMediator()
+        {
+            _listUser = new List<UserAbstract>();
+        }
+
+        public void RegisterUser( UserAbstract user )
+        {
+            user.RegisterInstagramMediator( this );
+            _listUser.Add( user );
+        }
+
+        public void SendMessageToUser( string msg, UserAbstract sender )
+        {
+            foreach ( var user in _listUser )
+            {
+                if ( user != sender )
+                {
+                    user.ReceiveFromInstagram( msg );
+                }
+            }
+        }
+    }
+}
