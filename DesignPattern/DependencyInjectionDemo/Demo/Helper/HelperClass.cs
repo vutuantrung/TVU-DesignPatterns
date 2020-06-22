477d7c716d6180a66c460a29639e4ad5c5090504
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-22 20:54:28 +0200

    Add Helper class

---
+++ DesignPattern/DependencyInjectionDemo/Demo/Helper/HelperClass.cs
@@ -0,0 +1,29 @@
+﻿using DependencyInjectionDemo.Demo.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionDemo.Demo.Helper
+{
+    public static class HelperClass
+    {
+        public static List<Employee> GetDynamicEmployeeList()
+        {
+            try
+            {
+                List<Employee> list = new List<Employee>();
+
+                list.Add( new Employee { ID = 0, Name = "NameA", Departement = "DepartementA" } );
+                list.Add( new Employee { ID = 0, Name = "NameB", Departement = "DepartementB" } );
+                list.Add( new Employee { ID = 0, Name = "NameC", Departement = "DepartementC" } );
+                list.Add( new Employee { ID = 0, Name = "NameD", Departement = "DepartementD" } );
+
+                return list;
+            }
+            catch ( Exception ex )
+            {
+                throw;
+            }
+        }
+    }
+}
