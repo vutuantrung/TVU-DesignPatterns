0417f5cc11ace8979b6a6e30168a1979ba23d799
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-22 22:04:06 +0200

    Update Unittests

--- DesignPattern/DependencyInjectionUnittests/PropertyDIUnittest.cs
+++ DesignPattern/DependencyInjectionUnittests/PropertyDIUnittest.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
-using PropertyAndMethodDI.PropertyDemo.BL;
-using PropertyAndMethodDI.PropertyDemo.Class;
-using PropertyAndMethodDI.PropertyDemo.DAL;
+using PropertyAndMethodDI.Demo.BL;
+using PropertyAndMethodDI.Demo.Class;
+using PropertyAndMethodDI.Demo.DAL;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,7 +12,7 @@ namespace DependencyInjectionUnittests
     class PropertyDIUnittest
     {
         [Test]
-        public void Test_Demo()
+        public void Test_Property_Demo()
         {
             List<Student> list = new List<Student>();
 
@@ -46,6 +46,43 @@ namespace DependencyInjectionUnittests
             {
                 Assert.Throws<ArgumentNullException>( () => studentBL.GetStudent( list, string.Empty ), "Name of name" );
                 Assert.Throws<ArgumentNullException>( () => studentBL.GetStudent( null, "StudentA" ), "Name of list" );
+            }
+        }
+
+        [Test]
+        public void Test_Method_Demo()
+        {
+            List<Student> list = new List<Student>();
+
+            list.Add( new Student { ID = 0, Name = "StudentNameA", SchoolName = "SchoolNameA" } );
+            list.Add( new Student { ID = 1, Name = "StudentNameB", SchoolName = "SchoolNameA" } );
+            list.Add( new Student { ID = 2, Name = "StudentNameC", SchoolName = "SchoolNameD" } );
+            list.Add( new Student { ID = 3, Name = "StudentNameD", SchoolName = "SchoolNameD" } );
+
+            StudentBL studentBL = new StudentBL();
+
+            {
+                var s = studentBL.GetStudent( new StudentDAL(), list, "StudentNameA" );
+                Assert.That( s.ID, Is.EqualTo( 0 ) );
+                Assert.That( s.Name, Is.EqualTo( "StudentNameA" ) );
+                Assert.That( s.SchoolName, Is.EqualTo( "SchoolNameA" ) );
+            }
+
+            {
+                var s = studentBL.GetStudent( new StudentDAL(), list, "StudentNameC" );
+                Assert.That( s.ID, Is.EqualTo( 2 ) );
+                Assert.That( s.Name, Is.EqualTo( "StudentNameC" ) );
+                Assert.That( s.SchoolName, Is.EqualTo( "SchoolNameD" ) );
+            }
+
+            {
+                var s = studentBL.GetStudent( new StudentDAL(), list, "StudentC" );
+                Assert.That( s, Is.EqualTo( null ) );
+            }
+
+            {
+                Assert.Throws<ArgumentNullException>( () => studentBL.GetStudent( new StudentDAL(), list, string.Empty ), "Name of name" );
+                Assert.Throws<ArgumentNullException>( () => studentBL.GetStudent( new StudentDAL(), null, "StudentA" ), "Name of list" );
             }
         }
     }
