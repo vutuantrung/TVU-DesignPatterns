c4fded15d40601dca7604eec840ad181f1c8499a
VU Tuan Trung <trung.lespace@gmail.com>
2020-06-22 20:54:22 +0200

    Add class object

---
+++ DesignPattern/DependencyInjectionDemo/Demo/Class/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionDemo.Demo.Class
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Departement { get; set; }
+    }
+}
